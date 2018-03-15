8144da6616090991b8a5128d81f8086dd62f71c8
Marianne Benkamoun <marianne.benkamoun@gmail.com>
2018-03-15 17:53:27 +0100

    added all the services

--- app/models/subservice.rb
+++
@@ -1,5 +0,0 @@
-class Subservice < ApplicationRecord
-  belongs_to :service
-  belongs_to :parent, :class_name => 'Subservice'
-  has_many :children, :class_name => 'Subservice', :foreign_key => 'subservice_id'
-end
