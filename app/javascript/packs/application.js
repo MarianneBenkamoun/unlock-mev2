075b375a06ddd79689b12207f15e27f59745a19e
Marianne Benkamoun <marianne.benkamoun@gmail.com>
2018-03-23 15:33:32 +0100

    fixed responsive

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,12 +14,12 @@ window.$ = $;
 require.context("images", true);
 
 
-import 'bootstrap/scss/bootstrap.scss';
-import 'font-awesome/scss/font-awesome.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 
 
 
-import 'src/application.scss';
+import 'src/application.css';
 
 
 
